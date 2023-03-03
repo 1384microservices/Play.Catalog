e58b796225d298cf9293ee5f4a2b9ae742fe97cc
Andrei Tudor <andrei.tudor@hotmail.com>
2023-03-03 17:50:49 +0200

    Cleanup

--- src/Play.Catalog.Service/Entities/Item.cs
+++ src/Play.Catalog.Service/Entities/Item.cs
@@ -6,9 +6,7 @@ public class Item : IEntity
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
-
     public string Description { get; set; } = string.Empty;
-
     public decimal Price { get; set; }
     public DateTimeOffset CreatedDate { get; set; }
 }
